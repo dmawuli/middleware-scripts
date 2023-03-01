1f45b86de001f828edab5c3de6156275acd807a3
dmawuli <123766047+dmawuli@users.noreply.github.com>
2023-03-01 15:43:57 -0500

    Create sonarinstall.sh

---
+++ sonarinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+su - vagrant
+echo "vagrant"
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+cd /opt
+sudo yum install wget -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64 
+ ./sonar.sh start
+
